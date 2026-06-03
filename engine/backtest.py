db2b5ac90dd35f896a1049cf567ea86a5c14c45a
reyatt30 <reyatt30@gmail.com>
2026-06-03 00:51:35 -0400

    Storing as series

--- engine/backtest.py
+++ engine/backtest.py
@@ -4,9 +4,9 @@ import pandas as pd
 
 def run_backtest(data, signals):
     start_amount = 10000
-    cash = start_amount
-    shares = 0
-    last_price = 0
+
+    # Convert signals to 1 (buy), 0 (sell), NaN (hold)
+    positions = pd.Series(np.where(signals == "buy", 1, np.where(signals == "sell", 0, np.nan)))
 
     for price, signal in zip(data["Close"], signals):
         if signal == "buy":
